8a6451c41a8c627adee16ccec2b70696f32ea79c
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-11 12:34:44 +0300

    Add task2

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Factorial
+
+double Factorial(int n)
+{
+    if ((n == 0) || (n == 1)) return 1;
+    else return n * Factorial(n - 1);
+}
+
+for (int i = 0; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
